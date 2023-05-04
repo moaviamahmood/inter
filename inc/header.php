95a4766baf3bc20d26fb1680e0ced22d85b60d8c
Moavia Mahmood <117826274+moaviamahmood@users.noreply.github.com>
2023-05-04 16:48:48 +0300

    Add files via upload

---
+++ inc/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="assets/css/bootstrap.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="assets/fontawesome/css/all.css"> 
+        <script type="text/javascript" scr="assets/js/jquery-1.12.4.js"> </script>
+        <script src ="assets/js/bootstrap.js"> </script>
+    </head>
+    <body>
+        <nav class="navbar navbar-dark bg-primary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.php" style="color:cornsilk;font-size:large;">Tourism Company</a>
+            </div>
+        </nav>
+    </body>
+</html>
